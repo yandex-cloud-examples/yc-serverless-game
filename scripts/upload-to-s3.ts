7dfd2ea01054a75937d5b750399d951e57692fe6
Ivan Zuev <dajohnes@gmail.com>
2022-08-23 12:52:19 +0300

    feat: static upload script

---
+++ scripts/upload-to-s3.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import fg from 'fast-glob';
+import mimeTypes from 'mime-types';
+
+const FILES_BASE_DIR = path.resolve('./dist/client');
+
+const client = new S3Client({
+    region: 'ru-central1',
+    endpoint: 'https://storage.yandexcloud.net',
+});
+
+const uploadFile = (filePath: string) => {
+    const contentType = mimeTypes.lookup(filePath) || 'text/plain';
+
+    const command = new PutObjectCommand({
+        Bucket: 'serverless-game-files',
+        Key: path.relative(FILES_BASE_DIR, filePath),
+        Body: fs.createReadStream(filePath),
+        ContentType: 'text/html',
+    });
+
+    return client.send(command);
+};
+
+const files = fg.sync('**/*', {
+    cwd: path.resolve(FILES_BASE_DIR),
+    absolute: true,
+});
+
+for (const file of files) {
+    uploadFile(file);
+}
