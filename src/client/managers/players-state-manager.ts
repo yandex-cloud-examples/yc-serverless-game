894f0539d590b05d2bc56d633d49b6332de57f58
Ivan Zuev <dajohnes@gmail.com>
2022-08-25 14:45:04 +0300

    feat: players state manager

---
+++ src/client/managers/players-state-manager.ts
@@ -0,0 +1,76 @@
+import * as phaser from 'phaser';
+import { autorun } from 'mobx';
+import { bind } from 'bind-decorator';
+
+import { Player } from '../objects/player';
+import { GameState } from '../state/game-state';
+import { AssetKeys } from '../assets';
+import { GridCoords } from '../objects/grid/grid-coords';
+
+export class PlayersStateManager {
+    private me: Player | undefined;
+    // ID -> Player map
+    private enemies: Map<string, Player> = new Map();
+
+    constructor(
+        private gameState: GameState,
+        private scene: phaser.Scene,
+    ) {
+        autorun(this.updateMeState);
+        autorun(this.updatePlayersState);
+    }
+
+    getMe(): Player {
+        if (!this.me) {
+            throw new Error('Me player is not set up yet');
+        }
+
+        return this.me;
+    }
+
+    @bind
+    private updateMeState() {
+        const meState = this.gameState.me;
+
+        if (!this.me) {
+            this.me = new Player(this.scene, AssetKeys.Player, meState.color, meState.gridX, meState.gridY);
+        } else {
+            const meCoords = this.me.getGridPos();
+
+            if (!GridCoords.equals(meCoords, [meState.gridX, meState.gridY])) {
+                this.me.moveToGridCell(meState.gridX, meState.gridY);
+            }
+        }
+    }
+
+    @bind
+    private updatePlayersState() {
+        const playersState = this.gameState.players;
+
+        for (const state of playersState) {
+            let player: Player | undefined = this.enemies.get(state.id);
+
+            if (!player) {
+                player = new Player(this.scene, AssetKeys.Player, state.color, state.gridX, state.gridY);
+
+                this.enemies.set(state.id, player);
+            }
+
+            if (!GridCoords.equals(player.getGridPos(), [state.gridX, state.gridY])) {
+                player.moveToGridCell(state.gridX, state.gridY);
+            }
+        }
+
+        // Remove players which are not in state anymore
+        for (const entry of this.enemies) {
+            const id = entry[0];
+            const player = entry[1];
+
+            if (!playersState.some((p) => p.id === id)) {
+                this.enemies.delete(id);
+
+                player.destroy();
+            }
+        }
+    }
+}
