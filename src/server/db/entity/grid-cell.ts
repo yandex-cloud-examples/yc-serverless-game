5be834924df57695154a44d8af3cd322b57eabfe
Ivan Zuev <dajohnes@gmail.com>
2022-11-14 11:12:55 +0300

    feat: paginated grid cells query

--- src/server/db/entity/grid-cell.ts
+++ src/server/db/entity/grid-cell.ts
@@ -1,5 +1,5 @@
 import {
-    withTypeOptions, snakeToCamelCaseConversion, declareType, Types, Session,
+    withTypeOptions, snakeToCamelCaseConversion, declareType, Types, Session, TypedValues,
 } from 'ydb-sdk';
 import { Entity } from './entity';
 import { executeQuery } from '../execute-query';
@@ -29,10 +29,41 @@ export class GridCell extends Entity {
         this.ownerId = data.ownerId;
     }
 
-    // TODO: handle case when there are more than 1000 rows
     static async all(dbSess: Session): Promise<GridCell[]> {
-        const { resultSets } = await executeQuery(dbSess, 'SELECT * FROM GridCells');
+        const LIMIT = 1000;
+        const result: GridCell[] = [];
 
-        return this.fromResultSet(resultSets[0]);
+        let lastX = -1;
+        let lastY = -1;
+        let hasMore = true;
+
+        while (hasMore) {
+            const { resultSets } = await executeQuery(dbSess, `
+                DECLARE $limit AS Uint32;
+                DECLARE $lastX AS Int64;
+                DECLARE $lastY AS Int64;
+                
+                SELECT * FROM GridCells WHERE x > $lastX OR (x == $lastX AND y > $lastY) ORDER BY x, y LIMIT $limit;
+            `, {
+                $limit: TypedValues.uint32(LIMIT),
+                $lastX: TypedValues.int64(lastX),
+                $lastY: TypedValues.int64(lastY),
+            });
+
+            const gridCells = this.fromResultSet(resultSets[0]);
+
+            if (gridCells.length > 0) {
+                const lastGridCell = gridCells[gridCells.length - 1];
+
+                lastX = lastGridCell.x;
+                lastY = lastGridCell.y;
+
+                result.push(...gridCells);
+            } else {
+                hasMore = false;
+            }
+        }
+
+        return result;
     }
 }
